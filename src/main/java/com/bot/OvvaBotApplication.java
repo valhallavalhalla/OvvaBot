cf030093b804bf3301963afe0f63a68a9f6e4fce
valhallavalhalla <shavinskiy.andrew@gmail.com>
2017-03-05 03:46:15 +0200

    first commit

---
+++ src/main/java/com/bot/OvvaBotApplication.java
@@ -0,0 +1,13 @@
+package com.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvvaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvvaBotApplication.class, args);
+	}
+
+}
